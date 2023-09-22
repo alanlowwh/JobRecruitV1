d4a4061c90a8fbe236b86112d13f9bfe4e167212
Alan <lowwaihorng@gmail.com>
2023-09-22 12:27:26 +0800

    update button location

--- app/src/main/java/com/example/jobrecruitv1/fragments/add/AddJobFragment.kt
+++ app/src/main/java/com/example/jobrecruitv1/fragments/add/AddJobFragment.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.jobrecruitv1.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 
 class AddJobFragment : Fragment() {
@@ -16,7 +18,13 @@ class AddJobFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_job, container, false)
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_add_job, container, false)
+
+
+
+
+        return view
     }
 
 }
